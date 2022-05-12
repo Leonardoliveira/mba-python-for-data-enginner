1b958a853e29f02bdd0379bad3ce4356133b6f31
pabloxcastro <pablo.castro@hotmail.com>
2022-05-11 23:00:56 -0300

    fix iniciando filtro

--- Pablo/aula3_teste.py
+++ Pablo/aula3_teste.py
@@ -26,20 +26,18 @@ if __name__ == "__main__":
     find_doi = config["find"]["doi"]
     find_jcr_value = config["find"]["jcr_value"]
     find_scimago_value = config["find"]["scimago_value"]
-
-    """ EXTRACT AULA 3"""
+    
     # carrega a lista de arquivos alvos do ETL
     files = get_files(input_file, "CSV")
 
     lista_df = []
-
-    """ TRANSFORMATION AULA 3"""
+    
     # para cada arquivo repetir
     for file in files:
 
         # ler
         df_file = read_file_csv(input_file, file)
-
+                
         # padronizar coluna title
         if file[0:3] == "jcs":
             df_file.rename(columns = {"Full Journal Title": "title"}, inplace = True)
@@ -48,7 +46,7 @@ if __name__ == "__main__":
 
         # convertendo dados da coluna para maiusculo
         df_file['title'] = df_file['title'].str.upper()
-
+        
         # guarda o df na lista
         lista_df.append(df_file)
 
@@ -57,16 +55,13 @@ if __name__ == "__main__":
 
     # padronizar colunas
     df_class_3 = select_col(df_class_3, columns_aula3)
-
+  
     # deduplicacao de dados usando todas colunas
     df_class_3.drop_duplicates(keep = "first", inplace = True)
     df_class_3.to_excel('output/df_class_3' + '.xlsx', index = False)
-
-    """ EXTRACT AULA 2 """
+   
     # ler CSV da aula 2
     df_class_2 = read_file_csv(output_file, file_class_2 + "." + export_to) 
-    
-    """ TRANSFORMATION AULA 2 """
     df_class_2['title'] = df_class_2['title'].str.upper()
     df_class_2.drop_duplicates(keep = "first", inplace = True)
     df_class_2.fillna("BD", inplace = True)
@@ -87,9 +82,9 @@ if __name__ == "__main__":
     
     if find_abstract is not None:
         find_abstract = df_final.abstract.str.contains(find_abstract)
-        df_final = df_final.loc[find_abstract]     
+        df_final = df_final.loc[find_abstract]             
 
     df_final.to_excel('output/df_final' + '.xlsx', index = False)
-    print("Resultado {} linhas encontradas".format(df_final.shape[0]))
+    print("Resultado {} linhas encontradas".format(df_final.shape[0]))   
                         
     
